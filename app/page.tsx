662ebda43c9b932b7c569835140ddefd4f230576
Shota Shimizu <tano.sukenyabu@gmail.com>
2025-08-12 10:23:51 +0900

    use link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="p-8 space-y-6">
       <h1 className="text-2xl font-semibold">poli-money-something</h1>
       <p>
         Try the demo Sankey chart at{' '}
-        <a className="text-blue-600 underline" href="/hoge">
+        <Link className="text-blue-600 underline" href="/hoge">
           /hoge
-        </a>
+        </Link>
         .
       </p>
     </main>
