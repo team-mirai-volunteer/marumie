1608968685486c335866a3f05e2e41c5e9879f25
Jun Ito <jujunjun110@gmail.com>
2025-08-18 16:59:02 +0900

    Update: list page

---
+++ webapp/src/server/actions/get-all-political-organizations.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaPoliticalOrganizationRepository } from "@/server/repositories/prisma-political-organization.repository";
+import type { PoliticalOrganization } from "@/shared/models/political-organization";
+
+const prisma = new PrismaClient();
+
+export async function getAllPoliticalOrganizations(): Promise<PoliticalOrganization[]> {
+  const repository = new PrismaPoliticalOrganizationRepository(prisma);
+  return await repository.findAll();
+}
