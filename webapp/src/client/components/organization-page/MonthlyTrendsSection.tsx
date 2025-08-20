cc774462d254f8ad2e3af5cc0326a13c6e08662e
Jun Ito <jujunjun110@gmail.com>
2025-08-20 15:26:18 +0900

    Update: graph data

--- webapp/src/client/components/organization-page/MonthlyTrendsSection.tsx
+++ webapp/src/client/components/organization-page/MonthlyTrendsSection.tsx
@@ -14,7 +14,9 @@ interface MonthlyTrendsSectionProps {
   monthlyData?: MonthlyData[];
 }
 
-export default function MonthlyTrendsSection({ monthlyData }: MonthlyTrendsSectionProps) {
+export default function MonthlyTrendsSection({
+  monthlyData,
+}: MonthlyTrendsSectionProps) {
   return (
     <MainColumnCard>
       <CardHeader
@@ -34,15 +36,8 @@ export default function MonthlyTrendsSection({ monthlyData }: MonthlyTrendsSecti
       {/* 月次データ表示 */}
       {monthlyData && monthlyData.length > 0 ? (
         <div className="space-y-4">
-          <div className="h-[300px] bg-gray-50 rounded-lg flex items-center justify-center">
-            <div className="text-center text-gray-500">
-              <div className="text-lg font-medium mb-2">月次収支推移グラフ</div>
-              <div className="text-sm">実装予定</div>
-            </div>
-          </div>
-          
           <div className="bg-gray-50 p-4 rounded-lg">
-            <h4 className="font-medium mb-2">月次データ（JSON形式）:</h4>
+            <h4 className="font-medium mb-2">Data</h4>
             <pre className="text-xs bg-white p-3 rounded border overflow-auto max-h-32">
               {JSON.stringify(monthlyData, null, 2)}
             </pre>
