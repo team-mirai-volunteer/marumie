73af5d536166d41b27bbda21679356c5141c6304
Jun Ito <jujunjun110@gmail.com>
2025-09-19 10:56:35 +0900

    Update: もとのtoppageを削除

--- webapp/src/app/not-found.tsx
+++ webapp/src/app/not-found.tsx
@@ -2,5 +2,6 @@ import "server-only";
 import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  redirect("/");
+  // 遷移先で適切なslugを取得できるのでここではdummy_valueで固定
+  redirect("/o/dummy_value");
 }
