9e1bff3099ca4273de41441fddc9d98ade937cd7
Jun Ito <jujunjun110@gmail.com>
2025-10-01 21:39:11 +0900

    Update: back button

--- webapp/src/client/components/common/FloatingBackButton.tsx
+++ webapp/src/client/components/common/FloatingBackButton.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface FloatingBackButtonProps {
   slug: string;
 }
 
 export default function FloatingBackButton({ slug }: FloatingBackButtonProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/o/${slug}`);
+  };
+
   return (
-    <Link
-      href={`/o/${slug}`}
+    <button
+      type="button"
+      onClick={handleClick}
       className="fixed bottom-6 left-6 z-50 flex h-[60px] w-[60px] items-center justify-center rounded-full bg-white shadow-[2px_4px_10px_0px_rgba(144,144,144,0.25)] border-[0.5px] border-black md:hidden"
     >
       <Image
@@ -20,6 +27,6 @@ export default function FloatingBackButton({ slug }: FloatingBackButtonProps) {
         height={17}
         className="text-gray-600"
       />
-    </Link>
+    </button>
   );
 }
